adc9617d7dee3d55846add2e5d248937edda57d0
ALicia Mulder <alicia.mulder@gmail.com>
2018-05-10 03:51:49 +0200

    Added the graph that links packages and connections

--- GroupProject/Pages/Index.cshtml.cs
+++ GroupProject/Pages/Index.cshtml.cs
@@ -4,11 +4,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using GroupProject.Data;
+using GroupProject.Entity;
 
 namespace GroupProject.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly GroupProject.Data.ISPContext _context;
+
+        public IndexModel(GroupProject.Data.ISPContext context)
+        {
+            _context = context;
+        }
+
         public void OnGet()
         {
 
